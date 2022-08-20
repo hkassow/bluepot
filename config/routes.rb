aac47caf45b1b5fd4f869886f45f6295cb55c7e0
Hunter Kassow <hunterkassow@protonmail.com>
2022-08-19 20:39:47 -0400

    removed unneccessary route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   delete '/follows', to: 'follows#destroy'
-  get '/hello', to: 'application#hello_world'
   post "/login", to: "sessions#create"
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
